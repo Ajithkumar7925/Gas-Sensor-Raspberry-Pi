03281ec5bbfd182b30ddfb4592d2cc49211cd8da
AJITH KUMAR P <72341402+Ajithkumar7925@users.noreply.github.com>
2021-10-30 14:00:17 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 from mq import *
 import sys, time
+from urllib.request import urlopen
+myAPI = '3CUMOGTM5V2JA2CL'
+baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
 
 try:
     print("Press CTRL+C to abort.")
@@ -10,8 +13,11 @@ try:
         sys.stdout.write("\r")
         sys.stdout.write("\033[K")
         sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+        conn = urlopen(baseURL + '&field1=%f' % (perc))
+        conn.close()
         sys.stdout.flush()
         time.sleep(0.1)
+        
 
 except:
     print("\nAbort by user")
